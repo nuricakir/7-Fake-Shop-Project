7f0e979214ed68eda5f917733061878402c31dee
Nuri ÇAKIR <112883476+nuricakir@users.noreply.github.com>
2023-04-09 20:18:02 +0300

    Add files via upload

---
+++ src/containers/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2 className="shoptitle">
+          <b> FAKE SHOP </b>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
